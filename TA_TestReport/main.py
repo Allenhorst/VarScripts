2f62bc0294daba8ad9e41be29c09e5794267c380
tsypunov <maxim.tsypunov@paragon-software.com>
2017-07-31 14:56:49 +0300

    v0.2 -  some fixes : remove existing file before usags; fixed incorrect success filed in cae where no successful runs were found

--- TA_TestReport/main.py
+++ TA_TestReport/main.py
@@ -2,9 +2,10 @@ import Teamcity as TC
 import os
 allBuildIds = []
 allBuildNames = []
-allTestsProjectIds = ["Prm_Tests", "Prm_Tests_L1", "Prm_Tests_L2", "Prm_Tests_L3"]
+allTestsProjectIds = ["Prm_Tests_L1", "Prm_Tests_L2", "Prm_Tests_L3"]
+excludeProjectIds = ["Investigate", "Legacy"] #to do add real id
 agentId = "ta-srv1162-f3"
-filename = "report_" + agentId +".html" 
+filename = "report_" + agentId +".html"
 startHTML = "\r\n<html> <body> \r\n<table border=1 cellpadding=10 cellspacing=0> <tr><tr> \r\n"
 endHTML = "\r\n\r\n</html> </body>\r\n</tr> </table>\r\n"
 
@@ -34,7 +35,12 @@ TC.Teamcity.__init__(TC.Teamcity,username="paragon\\tsypunov",
 #buildUrl, buildName, buildVer = TC.Teamcity.getInformationFromBuild(TC.Teamcity, build)
 #print("end")
 try:
-    f = open(filename,"w+")
+    os.remove(filename)
+except FileNotFoundError as e:
+    pass
+
+try:
+    f = open(filename, "w+")
 except OSError as e:
     pass
 
@@ -47,6 +53,11 @@ for Pr_ids in allTestsProjectIds:
    allBuildNames += buildnames
 
 print(agentId)
+allBuildIds.append("Prm_Tests_300smokeMainWithSdk")
+allBuildNames.append("300 SMOKE Main with SDK")
+allBuildIds.append("Prm_Tests_310unitUniversal")
+allBuildNames.append("310 UNIT Universal")
+
 #f = open(filename)
 f.write(startHTML)
 
@@ -57,12 +68,14 @@ for buildID in allBuildIds:
     if lastBuild:
         buildText = TC.Teamcity.getBuildById(TC.Teamcity, str(lastBuild))
         buildUrl, buildName, buildVer = TC.Teamcity.getInformationFromBuild(TC.Teamcity, buildText)
+        out = "<tr> <td valign=top>" + buildName + "</td> <td valign=top>  " + buildVer + "</td> <td valign=top> " + "<a href=\"" + buildUrl + "\" style=color: #9ACD32>" + "SUCCESS" + "</a>" + " \n"
     else:
         buildName = buildID
         buildVer = "N\A "
         buildUrl = "No builds of this kind"
+        out = "<tr> <td valign=top>" + buildName + "</td> <td valign=top>  " + buildVer + "</td> <td valign=top> " +  buildUrl +  " \n"
     output = "Build Name: " + buildName + " Version: " + buildVer + "URL: " +buildUrl
-    out = "<tr> <td valign=top>" + buildName + "</td> <td valign=top>  " + buildVer +"</td> <td valign=top> " +  buildUrl +  " \n"
+
     #print(output)
     f.write(out)
 
