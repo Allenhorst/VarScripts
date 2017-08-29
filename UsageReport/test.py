1ac6ca74709f4f9e837e1c7a066ebfe1b64e918f
tsypunov <maxim.tsypunov@paragon-software.com>
2017-08-29 17:33:23 +0300

    Added some wrappers to use in finding last report to compare with

--- UsageReport/test.py
+++ UsageReport/test.py
@@ -1,12 +1,55 @@
 import DiskUsage
 import loader
 import Reporter
+import sys
+import os
+import fnmatch
+
+"""
+consider input filename as rootId-yy-mm-dd-hh-mm-ss.json
+TO DO: need to rename it during build, or when reporter is running, this allows to find the most relevant report
+
+"""
+def findFile(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
+
+def findLastFile(files):
+
+    t= sorted(files).pop()
+    return  t
+
+
+curPath = os.getcwd()
+#find('*.txt', '/path/to/dir')
+
+
+
+
+
 
 DiskUsage.DiskUsage.__init__(DiskUsage.DiskUsage, username="adacc",
                          password="jd7gowuX",
                          server="http://vmsk-tc-prm.paragon-software.com",
                          port=None
                          )
+
+rootID = "_Root"
+try:
+    rootProjectId = sys.argv[1]
+except IndexError as e:
+    rootProjectId = rootID
+
+fileToFind = rootProjectId+".json"
+results = findFile(fileToFind, curPath+"//Reports")
+lastReport = ""
+
+
+
 
 L = loader.Loader("tree2.json")
 js = L.parseJson("tree2.json")
@@ -15,6 +58,7 @@ old = loader.Loader.addProjectNode(loader.Loader, jsondata=js )
 
 t = DiskUsage.DiskUsage.getAllBuildsArtsize(DiskUsage.DiskUsage, "Prm_Sandbox_2Sintsov")
 new = DiskUsage.DiskUsage.buildCustomTree(DiskUsage.DiskUsage, "Prm_Sandbox_2Sintsov")
+
 
 #L1 = loader.Loader("tree3.json")
 #js1 = L1.parseJson("tree3.json")
